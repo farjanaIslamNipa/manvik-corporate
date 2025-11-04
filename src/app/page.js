85ab597e17dc975775e6ea9c45b7ec0d4a905e0f
farjanaIslamNipa <nipa.farzana87@gmail.com>
2025-11-04 18:49:44 +0600

    Set global styles

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,31 @@
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-black/[0.96] text-white antialiased bg-grid-white/[0.02]">
-Manvik corporate
+    <div className="min-h-screen container space-y-8">
+      <div className="flex flex-wrap gap-x-2">
+        <div className="h-10 w-10 bg-[#393E46]"></div>
+        <div className="h-10 w-10 bg-[#948979]"></div>
+        <div className="h-10 w-10 bg-[#DFD0B8]"></div>
+        <div className="h-10 w-10 bg-[#543A14]"></div>
+        <div className="h-10 w-10 bg-[#F0BB78]"></div>
+        <div className="h-10 w-10 bg-[#FFF0DC]"></div>
+      </div>
+      <div className="space-y-5">
+        <div className="text-[#948979]">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero ipsum consequatur vitae facilis maiores est possimus, officia quam maxime. Maiores, numquam. Neque et quaerat, eos molestias excepturi facilis consectetur incidunt, quis fugiat veritatis ad. Blanditiis quo eos rerum adipisci delectus magnam, laudantium doloribus quia velit debitis cupiditate dignissimos aperiam. In cum, ipsum impedit ipsam eveniet fugit velit debitis quis, pariatur incidunt odio? A, dolorum dolorem deleniti ab itaque modi quidem eius hic quibusdam. Blanditiis ipsam atque quisquam nam inventore mollitia eius, aliquid earum voluptas doloribus aspernatur dolorum ab quaerat. Atque eum reprehenderit nostrum quas sequi harum tenetur illo dolorum nam!
+        </div>
+        <div className="text-[#c3bdb5]">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero ipsum consequatur vitae facilis maiores est possimus, officia quam maxime. Maiores, numquam. Neque et quaerat, eos molestias excepturi facilis consectetur incidunt, quis fugiat veritatis ad. Blanditiis quo eos rerum adipisci delectus magnam, laudantium doloribus quia velit debitis cupiditate dignissimos aperiam. In cum, ipsum impedit ipsam eveniet fugit velit debitis quis, pariatur incidunt odio? A, dolorum dolorem deleniti ab itaque modi quidem eius hic quibusdam. Blanditiis ipsam atque quisquam nam inventore mollitia eius, aliquid earum voluptas doloribus aspernatur dolorum ab quaerat. Atque eum reprehenderit nostrum quas sequi harum tenetur illo dolorum nam!
+        </div>
+        <div className="text-[#FFF0DC]">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero ipsum consequatur vitae facilis maiores est possimus, officia quam maxime. Maiores, numquam. Neque et quaerat, eos molestias excepturi facilis consectetur incidunt, quis fugiat veritatis ad. Blanditiis quo eos rerum adipisci delectus magnam, laudantium doloribus quia velit debitis cupiditate dignissimos aperiam. In cum, ipsum impedit ipsam eveniet fugit velit debitis quis, pariatur incidunt odio? A, dolorum dolorem deleniti ab itaque modi quidem eius hic quibusdam. Blanditiis ipsam atque quisquam nam inventore mollitia eius, aliquid earum voluptas doloribus aspernatur dolorum ab quaerat. Atque eum reprehenderit nostrum quas sequi harum tenetur illo dolorum nam!
+        </div>
+      </div>
+      <div>
+      set 1 : #222831, #393E46, #948979, #DFD0B8
+
+set 2 : #131010, #543A14, #F0BB78, #FFF0DC
+      </div>
     </div>
   );
 }
